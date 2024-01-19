0169f6884911520d823e1064fe5f211f1c618b6b
Md. Zayed Oyshik <zayedoyshik1@gmail.com>
2024-01-19 21:29:21 +0600

    Button Animation

--- lib/implicit_examples/animated_shopping_cart_button.dart
+++ lib/implicit_examples/animated_shopping_cart_button.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class ShoppingCartButton extends StatelessWidget {
+class ShoppingCartButton extends StatefulWidget {
   const ShoppingCartButton({super.key});
+
+  @override
+  State<ShoppingCartButton> createState() => _ShoppingCartButtonState();
+}
+
+class _ShoppingCartButtonState extends State<ShoppingCartButton> {
+  bool isExpanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -10,16 +17,39 @@ class ShoppingCartButton extends StatelessWidget {
         title: const Text('Shopping Cart'),
       ),
       body: Center(
-        child: Container(
-          width: 80.0,
-          height: 60.0,
-          decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          child: const Icon(
-            Icons.shopping_cart,
-            color: Colors.white,
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              isExpanded = !isExpanded;
+            });
+          },
+          child: AnimatedContainer(
+            width: isExpanded ? 250 : 80.0,
+            height: 60.0,
+            decoration: BoxDecoration(
+              color: isExpanded ? Colors.green : Colors.blue,
+              borderRadius: BorderRadius.circular(isExpanded ? 30.0 : 10),
+            ),
+            duration: const Duration(milliseconds: 500),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Icon(
+                    isExpanded ? Icons.add_shopping_cart : Icons.shopping_cart,
+                    color: Colors.white,
+                  ),
+                ),
+                if (isExpanded)
+                  Expanded(
+                      flex: 3,
+                      child: Text(
+                        "Added to the Cart",
+                        style: const TextStyle(fontSize: 20),
+                      )),
+              ],
+            ),
           ),
         ),
       ),
